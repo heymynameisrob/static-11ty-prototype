33192befa4983d6efc9fd1cb7940e4a9d3aba8bf
Rob Hough <robhough180@gmail.com>
2020-07-13 15:08:56 +0100

    base

---
+++ static/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _test = require("./test");
+
+(0, _test.Test)();
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Test = void 0;
+
+const Test = () => {
+  alert('Test');
+};
+
+exports.Test = Test;
